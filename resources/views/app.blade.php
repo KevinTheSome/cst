b371442702af2a6c4bbe89b14b96b48414d06f94
Adrians Raitums <126756100+bigboy3302@users.noreply.github.com>
2025-11-06 14:26:23 +0200

    blade

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,9 +7,9 @@
 
     <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
-    <link rel="icon" href="/favicon.ico" sizes="any">
-    <link rel="icon" href="/favicon.svg" type="image/svg+xml">
-    <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+    <link rel="icon" href="/bzl-site-icon-jauns-biezaka-kontura-01.png" sizes="any">
+    <link rel="icon" href="/bzl-site-icon-jauns-biezaka-kontura-01.png" type="image/svg+xml">
+    <link rel="apple-touch-icon" href="/bzl-site-icon-jauns-biezaka-kontura-01.png">
 
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
