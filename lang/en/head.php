e226095bec5d4b078c9dc14f9c59306ef350517e
JanisBramanisVTDTipb22 <127381689+ArtisanJanka@users.noreply.github.com>
2025-12-02 10:07:57 +0200

    navbar lang switch

--- lang/en/head.php
+++ lang/en/head.php
@@ -2,4 +2,24 @@
 
 return [
     'title' => 'Biochip scientific laboratory',
+    'nav_patients' => 'FOR PATIENTS',
+    'nav_patients_atmp' => 'What is ATMP?',
+    'nav_patients_psoriasis' => 'Psoriasis and new therapies',
+    'nav_patients_crohn' => 'Crohn’s disease and new therapies',
+    'nav_patients_faq' => 'Frequently asked questions (FAQ)',
+
+    'nav_specialists' => 'FOR SPECIALISTS',
+    'nav_specialists_laws' => 'EU regulation and laws',
+    'nav_specialists_plants' => 'ATMP manufacturing in Europe',
+    'nav_specialists_training' => 'Training',
+
+    'nav_research' => 'RESEARCH',
+    'nav_research_postdoc' => 'PostDoc form',
+    'nav_research_code' => 'Code form',
+
+    'nav_about' => 'ABOUT US',
+    'nav_about_team' => 'Team',
+    'nav_about_contacts' => 'Contacts',
+    'nav_about_join' => 'Get in touch',
+    'nav_about_lab' => 'Laboratory',
 ];
--- lang/lv/head.php
+++ lang/lv/head.php
@@ -2,4 +2,24 @@
 
 return [
     'title' => 'Biočipu zinātnisko laboratoriju',
+    'nav_patients' => 'PACIENTIEM',
+    'nav_patients_atmp' => 'Kas ir ATMP?',
+    'nav_patients_psoriasis' => 'Psoriāze un jaunās terapijas',
+    'nav_patients_crohn' => 'Krona slimība un jaunās terapijas',
+    'nav_patients_faq' => 'Biežāk uzdotie jautājumi (FAQ)',
+
+    'nav_specialists' => 'SPECIĀLISTIEM',
+    'nav_specialists_laws' => 'ES regula un likumi',
+    'nav_specialists_plants' => 'ATMP ražotnes Eiropā',
+    'nav_specialists_training' => 'Apmācība',
+
+    'nav_research' => 'PĒTNIECĪBA',
+    'nav_research_postdoc' => 'PostDock anketa',
+    'nav_research_code' => 'Koda anketa',
+
+    'nav_about' => 'PAR MUMS',
+    'nav_about_team' => 'Komanda',
+    'nav_about_contacts' => 'Kontakti',
+    'nav_about_join' => 'Sazinies',
+    'nav_about_lab' => 'Laboratorija',
 ];
