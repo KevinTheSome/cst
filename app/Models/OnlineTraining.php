005b08b672884ca687d16e9c1435aebae2e6f43b
JanisBramanisVTDTipb22 <127381689+ArtisanJanka@users.noreply.github.com>
2025-12-03 09:40:14 +0200

    migrācijas un modeļi

---
+++ app/Models/OnlineTraining.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class OnlineTraining extends Model
+{
+    protected $fillable = [
+        'title',
+        'description',
+        'slug',
+        'platform',
+        'url',
+        'starts_at',
+        'ends_at',
+        'is_active',
+    ];
+
+    protected $casts = [
+        'title' => 'array',
+        'starts_at' => 'datetime',
+        'ends_at' => 'datetime',
+        'is_active' => 'boolean',
+    ];
+
+    public function codes()
+    {
+        return $this->hasMany(OnlineCode::class);
+    }
+}
