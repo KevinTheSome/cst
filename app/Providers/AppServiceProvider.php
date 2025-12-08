b44899dca9ab3b7d5eb713baec735179a09ebac2
JanisBramanisVTDTipb22 <127381689+ArtisanJanka@users.noreply.github.com>
2025-12-08 13:17:04 +0200

    update to pulse admin

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Gate;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Gate::define('viewPulse', function ($user = null) {
+        return session()->get('is_admin', false) === true;
+    });
     }
 }
