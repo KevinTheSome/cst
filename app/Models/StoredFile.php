dcb3124c14aa83d73f399bc12d07bcf101e3c07d
Dripsters123 <129275287+Dripsters123@users.noreply.github.com>
2025-12-10 10:07:25 +0200

    upload file

---
+++ app/Models/StoredFile.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class StoredFile extends Model
+{
+    protected $fillable = [
+        'title_lv',
+        'title_en',
+        'path',
+        'mime_type',
+        'size',
+    ];
+
+    public function url()
+    {
+        return asset('storage/' . $this->path);
+    }
+}
---
+++ database/migrations/2025_12_10_072723_create_stored_files_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up()
+    {
+        Schema::create('stored_files', function (Blueprint $table) {
+        $table->id();
+        $table->string('title_lv')->nullable();
+        $table->string('title_en')->nullable();
+        $table->string('path');
+        $table->string('mime_type')->nullable();
+        $table->unsignedBigInteger('size')->nullable();
+        $table->timestamps();
+});
+
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('stored_files');
+    }
+};
