e9a69e9242354bea76a133c5a205e9e1af2e25bb
Elvis <126756049+Eksustov@users.noreply.github.com>
2026-01-06 12:35:26 +0200

    training has lv and en language support

---
+++ lang/en/training.php
@@ -0,0 +1,37 @@
+<?php
+
+return [
+
+    'create' => [
+
+        'pageTitle' => 'Create Training',
+        'pageSubtitle' => 'Configure the details for a new educational event.',
+        'layoutTitle' => 'Create Training',
+
+        'back' => 'Back to list',
+
+        'contentDetails' => 'Content details',
+
+        'titleLv' => 'Title (Latvian)',
+        'titleEn' => 'Title (English)',
+
+        'titleLvPlaceholder' => 'Training title in Latvian',
+        'titleEnPlaceholder' => 'Training title',
+
+        'required' => 'Required field',
+
+        'description' => 'Description',
+        'descriptionPlaceholder' => 'Enter a detailed description of the training module...',
+
+        'externalResources' => 'External resources',
+        'resourceUrl' => 'Resource URL',
+
+        'status' => 'Status',
+        'published' => 'Published',
+        'publishedHint' => 'Visible to students',
+
+        'saving' => 'Saving...',
+        'create' => 'Create training',
+    ],
+
+];
---
+++ lang/lv/training.php
@@ -0,0 +1,37 @@
+<?php
+
+return [
+
+    'create' => [
+
+        'pageTitle' => 'Izveidot apmācību',
+        'pageSubtitle' => 'Konfigurējiet jaunas apmācības informāciju.',
+        'layoutTitle' => 'Izveidot apmācību',
+
+        'back' => 'Atpakaļ uz sarakstu',
+
+        'contentDetails' => 'Satura informācija',
+
+        'titleLv' => 'Nosaukums (latviešu)',
+        'titleEn' => 'Nosaukums (angļu)',
+
+        'titleLvPlaceholder' => 'Apmācības nosaukums',
+        'titleEnPlaceholder' => 'Apmācības nosaukums angliski',
+
+        'required' => 'Obligāts lauks',
+
+        'description' => 'Apraksts',
+        'descriptionPlaceholder' => 'Ievadiet detalizētu apmācības moduļa aprakstu...',
+
+        'externalResources' => 'Ārējie resursi',
+        'resourceUrl' => 'Resursa URL',
+
+        'status' => 'Statuss',
+        'published' => 'Publicēts',
+        'publishedHint' => 'Redzams studentiem',
+
+        'saving' => 'Saglabā...',
+        'create' => 'Izveidot apmācību',
+    ],
+
+];
