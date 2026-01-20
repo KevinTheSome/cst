4827bb54e20d9d0eb9ab93c6a3f878a06e8a4975
JanisBramanisVTDTipb22 <127381689+ArtisanJanka@users.noreply.github.com>
2026-01-20 13:46:10 +0200

    Update to apmācības

---
+++ lang/lv/common.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'title' => 'Nosaukums',
+    'status' => 'Statuss',
+    'actions' => 'Darbības',
+
+    'view' => 'Skatīt',
+    'edit' => 'Rediģēt',
+    'delete' => 'Dzēst',
+    'create' => 'Izveidot',
+
+    'active' => 'Aktīvs',
+    'inactive' => 'Neaktīvs',
+
+    'untitled' => 'Bez nosaukuma',
+
+    'delete_confirm' => 'Vai tiešām vēlaties dzēst?',
+];
